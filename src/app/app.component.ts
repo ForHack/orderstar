fccd26369ee5e071016684202ac6a78ecbcbedf5
odiljon.o <odiljon.o@payme.uz>
2025-04-07 17:15:37 +0500

    updates
    * migration to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { UiButtonComponent } from '@shared/ui/button/ui-button.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, UiButtonComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [RouterOutlet, UiButtonComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
 }
